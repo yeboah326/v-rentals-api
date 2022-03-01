59f47ff4612e44230c4eaedc08bd0d8a59679305
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-03-01 15:04:21 +0000

    Changed the password for generated user to a fixed one

--- api/tests/data.py
+++ api/tests/data.py
@@ -1,15 +1,16 @@
+from random import choice, uniform
 from api.extensions import fake
 
 user_one = {
     "username": fake.user_name(),
     "email": fake.email(),
-    "password": fake.password(),
+    "password": "!XvDzRasj9",
 }
 
 user_two = {
     "username": fake.user_name(),
     "email": fake.email(),
-    "password": fake.password(),
+    "password": "!XvDzRasj9",
 }
 
 customer_one = {
@@ -24,4 +25,20 @@ customer_two = {
     "email": fake.email(),
     "name": fake.name(),
     "telephone": fake.msisdn()[3:],
+}
+
+vehicle_one = {
+    "name": fake.first_name_nonbinary(),
+    "cost_per_day": round(uniform(300, 700), 2),
+    "penalty_per_day": round(uniform(100, 200), 2),
+    "vehicle_type": choice(["saloon", "bus", "motor_cycle", "bicycle"]),
+    "rented": False,
+}
+
+vehicle_two = {
+    "name": fake.first_name_nonbinary(),
+    "cost_per_day": round(uniform(300, 700), 2),
+    "penalty_per_day": round(uniform(100, 200), 2),
+    "vehicle_type": choice(["saloon", "bus", "motor_cycle", "bicycle"]),
+    "rented": False,
 }
